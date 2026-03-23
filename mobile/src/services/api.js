00f98bc4ef00289cd0169a854ab512a4ffcbf46e
NguyenDat204 <nguyentiendat2911204@gmail.com>
2026-03-24 03:16:24 +0700

    Done insights

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Use 10.0.2.2 for Android Emulator to access host machine's localhost
-const BASE_URL = 'http://192.168.1.198:8080/api';
+const BASE_URL = 'http://192.168.1.188:8080/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
